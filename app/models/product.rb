4855cd6fe6cfb502bc2df0988f5db99829f0532e
Connor La Rocca <connorlarocca@hotmail.com>
2022-11-07 13:49:35 -0500

    added some model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    if price >= 10
+      return false
+    else
+      return true
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+  end
+
+  def total
+    sum = price * 1.09
+  end
 end
